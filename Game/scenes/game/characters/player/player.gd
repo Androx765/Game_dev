569f269bff5ec16d708eb2f1254fc0b78eaf5793
Maximilian Stinner <63010306+AlphaZero999@users.noreply.github.com>
2024-06-28 15:53:31 +0200

    add custom input actions

--- Game/scenes/game/characters/player/player.gd
+++ Game/scenes/game/characters/player/player.gd
@@ -1,7 +1,6 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
+@export var speed = 5.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -13,18 +12,23 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	get_move_input(delta)
+	move_and_slide()
+	
+
+
+# handle the movement/deceleration.
+func get_move_input(delta):
+	
+	# Get the input direction
+	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 		
 		var angular_rotation = 10
 		mesh_node.rotation.y = lerp_angle(mesh_node.rotation.y, atan2(velocity.x, velocity.z) - rotation.y, delta * angular_rotation)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
