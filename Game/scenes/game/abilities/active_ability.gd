2ff3511470afdd70b9812079263c97699d515b65
Lilly <lillyschumann@googlemail.com>
2024-07-19 22:42:56 +0200

    Extend example ability

--- Game/scenes/game/abilities/active_ability.gd
+++ Game/scenes/game/abilities/active_ability.gd
@@ -3,7 +3,12 @@ extends Node3D
 
 var energy_cost: int
 var damage: int
-
+var source_entity: Entity
+var source_direction
+var animation: String
+var direction_of_travel: Vector3
+var speed: int
+var velocity = Vector3.RIGHT
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- Game/scenes/game/abilities/fire/fire.gd
+++ Game/scenes/game/abilities/fire/fire.gd
@@ -1,20 +1,36 @@
 extends ActiveAbility
 
-
-func execute(entity: Entity, direction = null):
-	if entity.energy_current >= 20:
-		print(entity.name + " casts FIRE")
-		entity.use_energy(20)
-	else:
-		print(entity.name + " cannot cast FIRE, ICH HAB KEIN MANA MEHR")
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# setup base stats
 	energy_cost = 20
 	damage = 4
-	# Replace with function body.
-
+	animation = "dance2"
+	speed = 2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+## execute this ability from [entity] in [direction]
+func execute(entity: Entity, direction = null, start_point = null):
+	source_entity = entity
+	source_direction = direction
+	# set start point if given
+	if start_point != null:
+		position = start_point
+	# if enough energy, fire ability
+	if entity.energy_current >= energy_cost:
+		fire_ability()
+		entity.use_energy(energy_cost)
+	else:
+		print(entity.name + " cannot cast FIRE: ICH HAB KEIN MANA MEHR")
+
+func fire_ability():
+	look_at(source_direction)
+	direction_of_travel = rotation
+	print(source_entity.name + " casts FIRE in direction " + str(rotation) + "\nwith start " + str(position))
+	#source_entity.anim_state.travel(animation)
+	
+func _physics_process(delta):
+	position += velocity * delta * speed
